adef44555cb2efed236ddb7384b281e7c53c5167
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2021-07-02 12:43:13 +0100

    update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,44 @@
 let pokemonList = [
-  { name: "Bulbasaur", height: 0.7, weight: 6.9, abilities: ["clorophyll","overgrow"], type: ["grass","poison",]}, 
-  { name: "Charizard", height: 1.7, weight: 90.5, abilities: ["blaze","solar-power"], type: ["fire","flying",]},
-  { name: "Butterfree", height: 1.1, weight: 32, abilities: ["compoundeyes","tinted-lens"], type: ["bug","flying",]},
-  { name: "Beedrill", height: 1, weight: 29.5, abilities: ["swarm","sniper"], type: ["bug","poison",]},
-  { name: "pidgeotto", height: 1.1, weight: 30, abilities: ["keen-eye","tangled-feet","big-pecks"], type: ["flying","normal",]},
-  { name: "Wigglytuff", height: 1, weight: 12, abilities: ["cute-charm","frisk"], type: ["fairy","normal",]}
-]
+  { 
+    name: "Bulbasaur", 
+    height: 0.7, 
+    weight: 6.9, 
+    abilities: ["clorophyll","overgrow"], 
+    type: ["grass","poison",]
+  }, 
+  { 
+    name: "Charizard", 
+    height: 1.7, 
+    weight: 90.5, 
+    abilities: ["blaze","solar-power"], 
+    type: ["fire","flying",]
+  },
+  { 
+    name: "Butterfree", 
+    height: 1.1, 
+    weight: 32, 
+    abilities: ["compoundeyes","tinted-lens"], 
+    type: ["bug","flying",]
+  },
+  { 
+    name: "Beedrill", 
+    height: 1, 
+    weight: 29.5, 
+    abilities: ["swarm","sniper"], 
+    type: ["bug","poison",]
+  },
+  { 
+    name: "pidgeotto", 
+    height: 1.1, 
+    weight: 30, 
+    abilities: ["keen-eye","tangled-feet","big-pecks"], 
+    type: ["flying","normal",]
+  },
+  { 
+    name: "Wigglytuff", 
+    height: 1, 
+    weight: 12, 
+    abilities: ["cute-charm","frisk"], 
+    type: ["fairy","normal",]
+  }
+];
