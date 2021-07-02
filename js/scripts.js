1b00170a2860ce4f09d7416fa3511d721d63901b
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2021-07-02 22:05:35 +0100

    Changed type variable name to "types"

--- js/scripts.js
+++ js/scripts.js
@@ -4,41 +4,41 @@ let pokemonList = [
     height: 0.7, 
     weight: 6.9, 
     abilities: ["clorophyll","overgrow"], 
-    type: ["grass","poison",]
+    types: ["grass","poison",]
   }, 
   { 
     name: "Charizard", 
     height: 1.7, 
     weight: 90.5, 
     abilities: ["blaze","solar-power"], 
-    type: ["fire","flying",]
+    types: ["fire","flying",]
   },
   { 
     name: "Butterfree", 
     height: 1.1, 
     weight: 32, 
     abilities: ["compoundeyes","tinted-lens"], 
-    type: ["bug","flying",]
+    types: ["bug","flying",]
   },
   { 
     name: "Beedrill", 
     height: 1, 
     weight: 29.5, 
     abilities: ["swarm","sniper"], 
-    type: ["bug","poison",]
+    types: ["bug","poison",]
   },
   { 
     name: "pidgeotto", 
     height: 1.1, 
     weight: 30, 
     abilities: ["keen-eye","tangled-feet","big-pecks"], 
-    type: ["flying","normal",]
+    types: ["flying","normal",]
   },
   { 
     name: "Wigglytuff", 
     height: 1, 
     weight: 12, 
     abilities: ["cute-charm","frisk"], 
-    type: ["fairy","normal",]
+    types: ["fairy","normal",]
   }
 ];
