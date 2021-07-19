dff54371ab7160bc8a8479761f3ebf654713e2b5
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2021-07-19 16:17:47 +0200

    Create element 'listItem' and 'button'

--- js/scripts.js
+++ js/scripts.js
@@ -53,6 +53,16 @@ let pokemonRepository = (function (){
   // Create addListItem function
   function addListItem(pokemon) {
     let pokemonList = document.querySelector('.pokemon-list');
+
+    let listItem = document.createElement('li');
+
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+
+    button.classList.add('button-class');
+
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
   }
 
   return {
