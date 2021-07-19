0063ba381e79ff2878d76c8932c2f65151171ba9
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2021-07-19 15:56:28 +0200

    Create addListItem function in IIFE and Call Function

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,41 @@
 let pokemonRepository = (function (){
   let pokemonList = [
-    { 
-      name: "Bulbasaur", 
-      height: 0.7, 
-      weight: 6.9, 
-      abilities: ["clorophyll","overgrow"], 
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      weight: 6.9,
+      abilities: ["clorophyll","overgrow"],
       types: ["grass","poison",]
-    }, 
-    { 
-      name: "Charizard", 
-      height: 1.7, 
-      weight: 90.5, 
-      abilities: ["blaze","solar-power"], 
+    },
+    {
+      name: "Charizard",
+      height: 1.7,
+      weight: 90.5,
+      abilities: ["blaze","solar-power"],
       types: ["fire","flying",]
     },
-    { 
-      name: "Butterfree", 
-      height: 1.1, 
-      weight: 32, 
-      abilities: ["compoundeyes","tinted-lens"], 
+    {
+      name: "Butterfree",
+      height: 1.1,
+      weight: 32,
+      abilities: ["compoundeyes","tinted-lens"],
       types: ["bug","flying",]
     },
-    { 
-      name: "Beedrill", 
-      height: 1, 
-      weight: 29.5, 
-      abilities: ["swarm","sniper"], 
+    {
+      name: "Beedrill",
+      height: 1,
+      weight: 29.5,
+      abilities: ["swarm","sniper"],
       types: ["bug","poison",]
     },
-    { 
-      name: "pidgeotto", 
-      height: 1.1, 
-      weight: 30, 
-      abilities: ["keen-eye","tangled-feet","big-pecks"], 
+    {
+      name: "Pidgeotto",
+      height: 1.1,
+      weight: 30,
+      abilities: ["keen-eye","tangled-feet","big-pecks"],
       types: ["flying","normal",]
     },
-    
+
   ];
 
   function getAll() {
@@ -48,48 +48,53 @@ let pokemonRepository = (function (){
     } else {
       console.log('Incorrect entry.');
     }
-    
+
+
+  // Create addListItem function
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon-list');
   }
 
   return {
     getAll: getAll,
-    add: add
+    add: add,
+    addListItem: addListItem
   };
 })();
 
 pokemonRepository.add(
-  { 
-    name: "Wigglytuff", 
-    height: 1, 
-    weight: 12, 
-    abilities: ["cute-charm","frisk"], 
+  {
+    name: "Wigglytuff",
+    height: 1,
+    weight: 12,
+    abilities: ["cute-charm","frisk"],
     types: ["fairy","normal",]
   }
 );
 console.log(pokemonRepository.getAll());
 
-pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write( "<p>" + pokemon.name 
-                 + "<br>" + " Height: " + pokemon.height 
-                 + ", Weight: " + pokemon.weight 
-                 + ",  Abilities: " + pokemon.abilities 
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
+  /*document.write( "<p>" + pokemon.name
+                 + "<br>" + " Height: " + pokemon.height
+                 + ", Weight: " + pokemon.weight
+                 + ",  Abilities: " + pokemon.abilities
                  + ", Types: " + pokemon.types + "<p>")
 
   if (pokemon.height > 1.5) {
     document.write("Wow, that's big!!")
-  }
-});
+  }*/
 
 
  /*for (let i = 0; i < pokemonList.length; i++) {
-  document.write( "<p>" + pokemonList[i].name 
-                 + "<br>" + " Height: " + pokemonList[i].height 
-                 + ", Weight: " + pokemonList[i].weight 
-                 + ",  Abilities: " + pokemonList[i].abilities 
+  document.write( "<p>" + pokemonList[i].name
+                 + "<br>" + " Height: " + pokemonList[i].height
+                 + ", Weight: " + pokemonList[i].weight
+                 + ",  Abilities: " + pokemonList[i].abilities
                  + ", Types: " + pokemonList[i].types + "<p>")
-  
+
   if (pokemonList[i].height > 1.5) {
     document.write("Wow, that's big!")
   }
 } */
-
