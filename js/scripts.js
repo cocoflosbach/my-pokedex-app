11f5fa0795981c3a28980eb784ea54e7ddc919c1
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2021-08-03 01:44:33 +0100

    Update showDetails function to call showModal

--- js/scripts.js
+++ js/scripts.js
@@ -90,6 +90,7 @@ let pokemonRepository = (function (){
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
       console.log(pokemon);
+      showModal(pokemon);
     });
   }
 
